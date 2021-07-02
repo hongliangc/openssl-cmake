52a1902e4e4f468d1129e7b50ddb4516c984372d
hongliangc <hongliangc@hwtc.com.cn>
2021-07-02 15:32:42 +0800

    修改编译选项

---
+++ mk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. ./setenv.sh
+basepath=$(cd `dirname $0`; pwd)
+
+if [ ! -d $basepath/Bin/build ]
+then
+	echo "make build dir"
+	mkdir $basepath/Bin/build
+fi
+
+cd $basepath/Bin/build
+if [ "$1" == "rebuild" ]
+then
+	echo "**********rebuild makefile"
+	rm -rf ./*
+fi
+
+echo "PATH:$PATH"
+cmake -DPLATFORM=Linux -DVERSION=Release ../../
+make -j8
---
+++ setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+source /opt/LTBOXCL02-linuxAPP/environment-setup-armv7a-vfp-neon-oe-linux-gnueabi
